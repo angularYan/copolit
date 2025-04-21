eb7bcdedddb40d9bc26fccf4cdaab4842f865d67
yan <yanwei.wang@holman.com>
2025-04-21 11:14:05 -0400

    cool

--- folder1/WeatherApp/src/WeatherApp/Program.cs
+++ folder1/WeatherApp/src/WeatherApp/Program.cs
@@ -19,11 +19,41 @@ namespace WeatherApp
                 Console.WriteLine($"Weather in {weatherInfo.City}:");
                 Console.WriteLine($"Temperature: {weatherInfo.Temperature}°C");
                 Console.WriteLine($"Condition: {weatherInfo.Condition}");
+                Console.WriteLine(GetWeatherAsciiArt(weatherInfo.Condition));
             }
             else
             {
                 Console.WriteLine("Could not retrieve weather data. Please check the city name.");
             }
+        }
+
+        static string GetWeatherAsciiArt(string condition)
+        {
+            return condition.ToLower() switch
+            {
+                "clear" => @"
+     \   /
+      .-.
+   ― (   ) ―
+      `-'
+     /   \
+    Clear Sky",
+                "rain" => @"
+     .-.
+    (   ).
+   (___(__)
+    ‘ ‘ ‘ ‘
+   ‘ ‘ ‘ ‘
+    Rainy",
+                "cloudy" => @"
+     .--.
+  .-(    ).
+ (___.__)__)
+    Cloudy",
+                _ => @"
+     ???
+  Unknown Weather"
+            };
         }
     }
 }
